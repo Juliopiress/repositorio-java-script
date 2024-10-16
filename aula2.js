69e0e9877d4e84bad7b124c23b0f029a56ba5c99
jose carlos <>
2024-10-15 21:39:43 -0300

    comit
    um pouco mais de java

---
+++ aula2.js
@@ -0,0 +1,46 @@
+// let aspassimples = 'Estamos criando uma strinfg com aspas simples';
+// //string em resumo são texto que podem ser ultilizados no javascript
+// //e pra ultiliza-lo podemos usar com aspas simples, duplas, ou string template
+
+// let aspasDuplas = "String com aspas Duplas";
+// //Exemplo de string com aspas Duplas
+
+// let templateLiteral = `String interpolation allows embedding expressions like ${1 + 1}.`;
+// //Essa é uma String Template, basicamente usados em caso onde há necessidade de 
+// //realizar uma ação aritimética (como exemplo) ou quando guardamos algumas variável dentro dela
+// //O símbolo ${} em JavaStript é usado para interpolar variáveis, ou seja para retorna o valor dela
+// //--------------------------------------------------------------------------------------------------------------
+
+// //Veja abaixo exemplo de como e quando usar as crases
+
+// let nome ="igor";
+
+// let idade = 24;// Numero usados sem aspas (simples ou duplas)
+
+// let mensagem = `Ola, meu nome é ${nome} e tenho ${idade} anos`;
+
+// console.log(templateLiteral);
+
+// //---------------------------------------------------------------------------------------------------------------
+
+// let primeiroNome ='john';
+
+// let ultimoNome = 'Doe';
+
+// let nomeCompleto= primeiroNome +' '+ ultimoNome;
+
+// let nomeCompletoTemplate = `${primeiroNome} ${ultimoNome}`;
+
+// console.log(nomeCompleto);
+
+//---------------------------------------------------------------------------------------------------------------
+
+let nomeUsuario = 'Suzana Pereira';
+let conta = 'Corrente';
+let cartao = 878296;
+// let informacoes = `Ola ${nomeUsuario} nós entramos em contato para confirmar se sua
+// conta é tipo ${corrente} e o numero do seu cartão é ${Cartao}`;
+
+let resultado = `Nome do cliente é ${nomeUsuario}, e seu tipo de conta ${algo}`;
+
+console.log(resultado); 
