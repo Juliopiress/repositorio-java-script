7b4f0424593239dbb44d60975bf07440aeca3add
jose carlos <>
2024-11-01 20:09:10 -0300

    aula javascript 10/11

---
+++ escrevendo sozin/script.js
@@ -0,0 +1,12 @@
+
+const typed = new Typed('#typed-text',
+{
+strings: ["Corinthians eliminado","vai pra serie B"],
+typedSpeed: 50, //Velocidade que será digitado nosso text
+
+backSpeed:25,// Velocidade de retorno do text
+loop: true,
+showCursor: false,
+fadeOut:true,
+
+});
