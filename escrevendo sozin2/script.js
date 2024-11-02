3f70f5aa649550f38e32d053c7c5e7012d4e8d73
jose carlos <>
2024-11-01 21:00:16 -0300

    aula java script 01/11

---
+++ escrevendo sozin2/script.js
@@ -0,0 +1,11 @@
+const typed = new Typed('#typed-text',
+    {
+    strings: ["Venha nós conhecer","preços imperdivéis"],
+    typedSpeed: 60, //Velocidade que será digitado nosso text
+    
+    backSpeed:25,// Velocidade de retorno do text
+    loop: true,
+    showCursor: false,
+    fadeOut:true,
+    
+    });
